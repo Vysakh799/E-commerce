0897a5684cbe95415db8ab93396df9636707bba7
vysakh799 <vysakh7995@gmail.com>
2024-05-27 18:00:09 +0530

    adding models

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+ 
+class product(models.Model):
+    Name=models.TextField()
+    type=models.TextField()
+    price=models.TextField()
+    offer_price=models.TextField()
+    description=models.TextField()
+    stock=models.TextField()
+    weight1=models.TextField()
+    weight2=models.TextField(null=True)
+    weight3=models.TextField(null=True)
+    weight4=models.TextField(null=True)
+    weight5=models.TextField(null=True)
+    image1=models.FileField()
+    image2=models.FileField()
+    image3=models.FileField()
+    image4=models.FileField()
