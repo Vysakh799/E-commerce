6d9735378de23607321fdee1e1e78c25c0c39346
Vysakh799 <vysakh7995@gmail.com>
2024-06-04 18:04:45 +0530

    adding login

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.index),
     path('products/<pk>',views.products),
     path('login',views.login),
+    path('logout',views.logout),
     path('signup',views.signup),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render,redirect
 from.models import *
 from django.contrib import messages
 
+def getuser(request):
+    user=False
+
+    if 'user' in request.session:
+            user=True
+    return user
 def filter(data,price):
     price2=[]
     for i in data:
@@ -19,17 +25,34 @@ def index(request):
     allp=filter(data2,price)
     data3=product.objects.all()[::-1][:3]
     addp=filter(data3,price)
-    return render(request,'index.html',{'data':data,'price':price2,'allp':allp,'addp':addp})
+    return render(request,'index.html',{'data':data,'price':price2,'allp':allp,'addp':addp,'user':getuser(request)})
 
 def products(request,pk):
     product1=product.objects.get(pk=pk)
     weights=weight.objects.filter(p_name=product1)
-    return render(request,'product.html',{'weights':weights,'product1':product1})
+    
+    return render(request,'product_copy.html',{'weights':weights,'product1':product1,'user':getuser(request)})
 
 def login(request):
-    return render(request,"login.html")
+   
+        if request.method=="POST":
+            username=request.POST['username']
+            password=request.POST['password']
+            try:
+                data=users.objects.get(username=username,password=password)
+                request.session['user']=username
+                messages.success(request, "Login successfully completed!") 
 
-def signup(request):
+            except:
+                messages.warning(request, "Incorrect username or password!") 
+        
+            return redirect(index)
+        else:
+            return render(request,"login.html")
+def logout(request):
+     del request.session['user']
+     return redirect(index)
+def signup(request):    
     if request.method=="POST":
         name=request.POST['name']
         phno=request.POST['phno']
